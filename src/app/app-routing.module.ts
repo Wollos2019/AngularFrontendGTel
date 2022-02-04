c364403e150dd879baaae9d22bba33e103270216
pascal setup <passy841@gmail.com>
2022-02-04 13:10:45 +0100

    add template rh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CommercialComponent } from './commercial/commercial.component';
 import { MainAppComponent } from './template/main-app/main-app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
+  {
+    path: 'rh',
+    loadChildren: () => import('./rh/rh.module').then((m) => m.RhModule),
+  },
   {
     path: '',
     component: MainAppComponent,
@@ -15,11 +22,15 @@ const routes: Routes = [
       { path: 'register', component: RegisterComponent },
       { path: 'loggout', component: LoggoutComponent },
       { path: 'commercial', component: CommercialComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'products', component: ProductComponent },
+      { path: 'search', component: SearchComponent },
+
+      { path: 'products/:id', component: ProductComponent },
+      { path: 'achats', component: ProductComponent },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
     ],
-  },
-  {
-    path: 'rh',
-    loadChildren: () => import('./rh/rh.module').then((m) => m.RhModule),
   },
 ];
 
--- src/app/rh/rh-routing.module.ts
+++ src/app/rh/rh-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainRhComponent } from './template/main-rh/main-rh.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainRhComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RhRoutingModule { }
+export class RhRoutingModule {}
