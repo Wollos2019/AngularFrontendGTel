1f523cda6a3d73e988fe2a8495f0f28085491c71
Wollos2019 <wollos2@yahoo.com>
2022-06-20 02:13:06 +0100

    C de commercial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { MainAppComponent } from './template/main-app/main-app.component';
 import { ProductComponent } from './product/product.component';
 import { DashboardComponent } from './template/dashboard/dashboard.component';
-import { CommercialComponent } from './commercial/commercial.component';
+import { CommercialComponent } from './Commercial/commercial.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
