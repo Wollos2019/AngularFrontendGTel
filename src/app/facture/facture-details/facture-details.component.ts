b3a2c4f1007a7ae35cc0138c86b544e04666b245
Wollos2019 <wollos2@yahoo.com>
2022-07-22 01:19:32 +0100

    Historique3

--- src/app/facture/facture-details/facture-details.component.ts
+++ src/app/facture/facture-details/facture-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommandeDetaisService } from 'src/app/Commercial/commandes/services/commande-detais.service';
-import { IFacture } from '../ifacture';
+import { Facture, IFacture } from '../ifacture';
 import { FactureService } from '../services/facture.service';
 
 @Component({
@@ -12,11 +12,13 @@ import { FactureService } from '../services/facture.service';
 export class FactureDetailsComponent implements OnInit {
 
   public invoices = [] as any;
-  public invoice  = <IFacture>{};
+  public invoice  = <Facture>{};
   public totals = [] as any;
   public facDet = [] as any;
   public montantT = 0;
-  public rabais = 0;
+  public reducPro = 0;
+  public rabaisPro = 0;
+  public rabaisT = 0;
   public tva = 0;
   public totalN = 0;
   public products = [] as any;
@@ -32,19 +34,21 @@ export class FactureDetailsComponent implements OnInit {
     const id: number = +this.route.snapshot.paramMap.get('id')!;
     // const id: any = this.route.snapshot.paramMap.get('id');
     console.log('id:', id);
-    this.servFac.list()
+    this.servFac.getOneFacture(id)
       .subscribe(response => {
         if (response) {
-          this.invoices = response;
-
-          for (var val of this.invoices) {
-            if (val.id == id) {
-              this.invoice = val;
-              console.log(this.invoice);
-            }
+          this.invoice = response;
+          console.log(this.invoice);
+          for(var val of this.invoice.commandes_detail){
+            this.montantT = this.montantT + val.prix;
+            this.rabaisT = (this.montantT*20 / 100);
+            this.tva = ((this.montantT-this.rabaisT)*18)/100;
+            this.totalN = this.montantT-this.rabaisT+this.tva;
           }
         }
     });
   }
+
+
 
 }
