c4fd544980613b086880deefdaa3d1e10464a706
Wollos2019 <wollos2@yahoo.com>
2022-08-02 22:51:51 +0100

    ClientService

--- src/app/client/services/client.service.ts
+++ src/app/client/services/client.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Client, Iclient } from '../client';
 
 @Injectable({
@@ -8,12 +9,21 @@ import { Client, Iclient } from '../client';
 })
 export class ClientService {
   private _url = 'http://localhost:8000/api/commercial/clients';
+  URL_COMMER = environment.URL_COMMER;
 
   constructor(private http:HttpClient) { }
 
 
   list () : Observable<any> {
     return this.http.get<any>(this._url);
+  }
+
+  getOneClient (id: number) : Observable<any> {
+    return this.http.get<any>(`${this.URL_COMMER}client/${id}`);
+  }
+
+  getAllCommande (id:number) : Observable<any> {
+    return this.http.get<any>(`${this.URL_COMMER}historique/commandes/${id}`);
   }
 
   add(client:Client) : Observable<Client> {
@@ -32,6 +42,14 @@ export class ClientService {
     console.log('ici');
     console.log(x);
     return this.http.get<Iclient>('http://localhost:8000/api/products/search/'+x);
+  }
+
+  uploadPhotoClient(clientId?: string, image?: any): Observable<Client> {
+    console.log(`${this.URL_COMMER}${clientId}/images`);
+    return this.http.post<Client>(
+      `${this.URL_COMMER}clients/${clientId}/images`,
+      image
+    );
   }
 
 }
